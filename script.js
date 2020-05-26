a513a643e63b0693bf1a1f958bf614eb474b0636
josh-weiss76 <59374753+josh-weiss76@users.noreply.github.com>
2020-05-26 16:52:53 -0400

    Add files via upload
    fixed script to display all astronauts

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 // TODO: add code here
 window.addEventListener("load", function(){
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
-        response.json().then(function(json){
-            let astronaut = document.getElementById("container");
-            astronaut.innerHTML = `
+        return response.json();
+    }).then(function(json){
+            const container = document.getElementById("container");
+            let astronauts = '';
+            for (astronaut of json){
+            astronauts += `
                 <div class = "astronaut">
                     <div class = "bio"
-                        <h3><b>${json[0].firstName} ${json[0].lastName}</b>
+                        <h3>${astronaut.id}) ${astronaut.firstName} ${astronaut.lastName}</h3>
                         <ul>
-                            <li>Hours in space: ${json[0].hoursInSpace}
-                            <li>Active: ${json[0].active}
-                            <li>Skills: ${json[0].skills}
+                            <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                            <li>Active: ${astronaut.active}</li>
+                            <li>Skills: ${astronaut.skills.join(",")}</li>
                         </ul>
                     </div>
-                    <img class="avatar" src=${json[0].picture}
-                </div>`;
-                             
-        });
+                    <img class="avatar" src="${astronaut.picture}"/>
+                </div>
+                `;
+            }
+        container.innerHTML = astronauts;                
     });
 });
